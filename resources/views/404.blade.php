e50d642fdc805eafa93fc88985412c08223ac716
Jarmo Troska <jarmo@troska.ee>
2024-05-04 12:34:02 +0000

    Add 404 page design

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -4,7 +4,13 @@
 
 <div class="card-container">
 
-    404
+    <div class="error-404">
+
+        <h1>Looks like we couldn't find what you were looking for.</h1>
+
+        <p>This page doesn't exist. Did you enter the correct URL?</p>
+
+    </div>
 
 </div>
 
